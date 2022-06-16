88b45833a7771869aee9f9f3700e1f8380b1e59b
Begin Sergey <begins.1986@mail.ru>
2022-06-16 23:54:17 +0300

    Add homework 3

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react'
 import Greeting from './Greeting'
+import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType>
+    addUserCallback: (name:string)=>void // need to fix any
 }
 
 // более простой и понятный для новичков
@@ -12,17 +13,31 @@ type GreetingContainerPropsType = {
 // более современный и удобный для про :)
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    const [name, setName] = useState<string>('')
+    const [error, setError] = useState<string>('')
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        if(e.currentTarget.value.trim()!==''){
+            setName(e.currentTarget.value.trim())
+        } else {
+            setName('')
+            setError('Title is required!')
+        }
+
     }
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        addUserCallback(name)
+        alert(`Hello ${name} !`)
+        setName('')
     }
 
-    const totalUsers = 0 // need to fix
+    const onEnter = (e:KeyboardEvent<HTMLInputElement>)=>{
+        if(e.key ==='Enter'){
+            addUser()
+        }
+    }
+
+    const totalUsers = users.length
 
     return (
         <Greeting
@@ -31,6 +46,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
+            onEnter={onEnter}
         />
     )
 }
